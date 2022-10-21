9a9df5aa1fd832d25646be2026ed9a83ba6b0392
jonathan <imaginary@email.com>
2022-10-21 09:33:55 +0200

    ex00 just

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jrinna <jrinna@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 11:06:33 by jrinna            #+#    #+#             */
-/*   Updated: 2022/10/20 09:47:23 by jrinna           ###   ########lyon.fr   */
+/*   Updated: 2022/10/21 09:33:09 by jrinna           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,14 @@ template < typename T >
 int	easyfind( const T& cont, const int i ) {
 	typename T::const_iterator	it = cont.cbegin();
 	typename T::const_iterator	ite = cont.cend();
-	int	j = 0;
-	while (it != ite)
+	typename T::const_iterator itt = std::find(it, ite, i);
+	size_t j = 0;
+	while (it != itt)
 	{
-		if (*it == i)
-			return (j);
 		j++;
 		it++;
 	}
+	if (it != ite)
+		return (j);
 	return (-1);
 }
